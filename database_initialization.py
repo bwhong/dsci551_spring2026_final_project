dd113c8edba102952b43f329778d1770266009ed
Brandon Hong <brandonhong@Brandon-Hongs-MacBook-Air.local>
2026-02-25 16:25:20 -0800

    testing

--- database_initialization.py
+++ database_initialization.py
@@ -8,5 +8,6 @@ def initialize_database():
     with open("schema.sql", "r") as f:
         table_sql_setup = f.read()
     conn.executescript(table_sql_setup)
-    
+    conn.commit()
+    conn.close()
     
--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from database_initialization import initialize_database
 
 if __name__ == "__main__":
     initialize_database()
+    print('Welcome to the Budget Management Application!')
+    input('Please enter your username! If your username does not exist, we will automatically create an account: \n')
+    
