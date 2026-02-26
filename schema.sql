a4f55e69f8b25475bfcece2d09ee72126960994c
Brandon Hong <brandonhong@Brandon-Hongs-MacBook-Air.local>
2026-02-25 16:11:51 -0800

    schema completed

--- schema.sql
+++ schema.sql
@@ -1,27 +1,27 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id INT PRIMARY KEY,
-    user_name VARCHAR(100) NOT NULL
+    user_id INTEGER PRIMARY KEY,
+    user_name TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS category (
-    category_id INT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    user_id INT NOT NULL REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS budget (
-    budget_id INT PRIMARY KEY,
-    category_id INT REFERENCES category(category_id),
+    budget_id INTEGER PRIMARY KEY,
+    category_id INTEGER REFERENCES categories(category_id),
     budget_amount double NOT NULL,
-    month DATE NOT NULL
-    user_id INT NOT NULL REFERENCES users(user_id)
+    month TEXT NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
-    transaction_id INT PRIMARY KEY,
+    transaction_id INTEGER PRIMARY KEY,
     amount DOUBLE NOT NULL,
     transaction_date DATE NOT NULL,
-    category_id INT NOT NULL REFERENCES category(category_id),
-    budget_id INT NOT NULL REFERENCES budget(budget_id), 
-    user_id INT NOT NULL REFERENCES users(user_id)
+    category_id INTEGER NOT NULL REFERENCES categories(category_id),
+    budget_id INTEGER NOT NULL REFERENCES budget(budget_id), 
+    user_id INTEGER NOT NULL REFERENCES users(user_id)
 );
