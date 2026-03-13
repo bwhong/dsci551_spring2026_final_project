c1621dcd244532aa9a160144f1ef61864e48327b
Brandon Hong <brandonhong@Brandon-Hongs-MacBook-Air.local>
2026-03-13 11:17:04 -0700

    updated main.py with user functions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from database_initialization import initialize_database
+from user import ask_username, validate_user
 
 if __name__ == "__main__":
     initialize_database()
     print('Welcome to the Budget Management Application!')
-    username = input('Please enter your username! If your username does not exist, we will automatically create an account: \n')
-    # check if username exists (lowercase and remove white space). if not, insert it into users table
+    username = ask_username()
+    user_id = validate_user(username)
     # press 1 to go to cateogry, press 2 to go to budget, press 3 to go to transactions, press 4 to go to summary statistics 
     # go to category and create categoryes -> create a default list (ex: press 1 to add/delete cateogires, press 2 to continue)
     # go to budget py file and create a budget based on categories for the month (ex: press 1 to add/delete budget, press 2 to continue)
